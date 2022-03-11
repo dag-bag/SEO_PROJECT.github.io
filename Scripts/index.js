eb7d11e5e8893b7e587920e74ea1555f5e5a72dd
dag-bag <virenderkumar23435@gmail.com>
2022-03-11 18:23:09 +0530

    Updata for seo

--- Scripts/index.js
+++ Scripts/index.js
@@ -171,3 +171,28 @@ menuBtn.addEventListener("click", () => {
 close.addEventListener("click", () => {
   navBar.style.left = "-100%";
 });
+
+// Lazy Loading
+
+(function () {
+  var elements = document.querySelectorAll("img[data-src]");
+  var index = 0;
+  var lazyLoad = function () {
+    if (index >= elements.length) return;
+    var item = elements[index];
+    if (this.scrollY + this.innerHeight > item.offsetTop) {
+      var src = item.getAttribute("data-src");
+      item.src = src;
+      item.addEventListener("load", function () {
+        item.removeAttribute("data-src");
+      });
+      index++;
+      lazyLoad();
+    }
+  };
+  var init = function () {
+    window.addEventListener("scroll", lazyLoad);
+    lazyLoad();
+  };
+  return init();
+})();
